e68502167c2c15ac308f54e33ceb22dd42fe1da2
cfunakoshi <cheynefunakoshi_2016@depauw.edu>
2016-11-19 19:52:09 -0800

    changed header

--- assets/app/header.component.ts
+++ assets/app/header.component.ts
@@ -3,11 +3,13 @@ import { ROUTER_DIRECTIVES } from "@angular/router";
 @Component({
     selector: 'my-header',
     template: `
+        <div class="row">
+            <div class="col-sm-12 top-header">
+                    <h1 id="title">Raincheck</h1>
+            </div>
+        </div>
         <header class="row">
             <nav class="col-sm-12">
-                <div class="logo">
-                    <a [routerLink]="['/']">Raincheck</a>
-                </div>
                 <ul class="nav-links">
                     <li><a [routerLink]="['/']">Friend List</a></li>
                     <li><a [routerLink]="['/auth']">Authentication</a></li>
@@ -18,9 +20,23 @@ import { ROUTER_DIRECTIVES } from "@angular/router";
     `,
     directives: [ROUTER_DIRECTIVES],
     styles: [`
+        .top-header{
+            background-color: #0e8dd6;
+            height: 100px;
+        }
+
+        #title {
+            width: 100%;
+            text-align: center;
+            color: white;
+            text-transform: uppercase;
+            font-family: Gadget;
+        }
+
         header {
             margin-bottom: 20px;
             border-bottom: solid 1px;
+            background-color: #06324c;
         }
 
         .logo {
@@ -33,6 +49,10 @@ import { ROUTER_DIRECTIVES } from "@angular/router";
             float: right;
             padding: 0;
             margin: 20px;
+        }
+
+        .nav-links>li>a {
+            color: white;
         }
 
         li {
